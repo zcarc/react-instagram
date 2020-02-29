292761e196856b98227209653bc20433d1bc5ce3
wrkbr <zcarc1@gmail.com>
2020-02-29 17:32:14 +0900

    리덕스 추가

---
+++ front/reducers/post.js
@@ -0,0 +1,20 @@
+
+const initialState = {
+    mainPosts: [],
+};
+
+const ADD_POST = 'ADD_POST';
+
+
+export default (state = initialState, action) => {
+
+    switch(action.type) {
+
+        case ADD_POST: {
+            return {
+                ...state,
+            }
+        }
+
+    }
+};
---
+++ front/reducers/user.js
@@ -0,0 +1,39 @@
+import {default} from "react-redux/lib/utils/Subscription";
+
+const initialState = {
+    isLoggedIn : false,
+    user : {},
+};
+
+const LOG_IN = 'LOG_IN';
+const LOG_OUT = 'LOG_OUT';
+
+// const action = {
+//     type: 'LOG_IN',
+//     data: {
+//         nickname: 'user01',
+//     },
+// };
+
+export default (state = initialState, action) => {
+
+  switch(action.type) {
+
+      case LOG_IN: {
+        return {
+            ...state,
+            isLoggedIn: true,
+            user: action.data,
+        }
+      }
+
+      case LOG_OUT: {
+        return {
+            ...state,
+            isLoggedIn: false,
+            user: null,
+        }
+      }
+
+  }
+};
