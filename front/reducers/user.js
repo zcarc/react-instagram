31fe5a90803ec1ac2dfc06029e4ef8401cf2d025
wrkbr <zcarc1@gmail.com>
2020-03-01 14:26:20 +0900

    리덕스 사가 추가

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -3,9 +3,11 @@ const initialState = {
     user : {},
 };
 
-const LOG_IN = 'LOG_IN';
-const LOG_OUT = 'LOG_OUT';
-const SIGN_UP = 'SIGN_UP';
+export const LOG_IN = 'LOG_IN';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+export const LOG_OUT = 'LOG_OUT';
+export const SIGN_UP = 'SIGN_UP';
 
 // const action = {
 //     type: 'LOG_IN',
---
+++ front/sagas/user.js
@@ -0,0 +1,33 @@
+import {all, takeLatest, fork, put} from 'redux-saga/effects';
+import {LOG_IN, LOG_IN_FAILURE, LOG_IN_SUCCESS} from "../reducers/user";
+
+function loginAPI() {
+
+}
+
+function* login() {
+
+    try {
+        yield call(loginAPI);
+        yield put({
+            type: LOG_IN_SUCCESS,
+        });
+
+    }catch (e) {
+        console.error(e);
+        yield put({
+            type: LOG_IN_FAILURE,
+        });
+    }
+
+}
+
+function* watchLogin() {
+    yield takeLatest(LOG_IN, login);
+}
+
+export default function* userSaga() {
+    yield all([
+        fork(watchLogin),
+    ]);
+}
