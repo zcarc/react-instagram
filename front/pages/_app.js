d51f9cbd91ffbf2db770560f03cb61af99fb274f
wrkbr <zcarc1@gmail.com>
2020-03-01 17:04:09 +0900

    eslint-confg-airbnb babel-eslint 추가

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,62 +1,63 @@
 import React from 'react';
-import Head from 'next/head'
-import AppLayout from "../components/AppLayout";
-import {GlobalStyle} from '../components/style/header'
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 
-import reducer from '../reducers/index'
-import {createStore, compose, applyMiddleware} from "redux";
-import {Provider} from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 
-import createSagaMiddleware from 'redux-saga'
-import rootSaga from "../sagas/index";
+import createSagaMiddleware from 'redux-saga';
+import reducer from '../reducers/index';
+import { GlobalStyle } from '../components/style/header';
+import AppLayout from '../components/AppLayout';
+import rootSaga from '../sagas/index';
 
 
 const withoutAppLayout = [
-    'Register',
-    'Login',
+  'Register',
+  'Login',
 ];
 
-const Main = ({Component, store}) => {
-    // console.dir(Component);
-    // console.log(Component.name);
+const Main = ({ Component, store }) =>
+// console.dir(Component);
+// console.log(Component.name);
 
 
-    return (
-        <>
-            <Provider store={store}>
-                <Head>
-                    <link rel="stylesheet" href="/style/reset.css"/>
-                </Head>
-                <GlobalStyle/>
-                {withoutAppLayout.includes(Component.name) ? <Component/> :
-                    <AppLayout>
-                        <Component/>
-                    </AppLayout>
-                }
-            </Provider>
-        </>
-    );
-};
-
+  (
+    <>
+      <Provider store={store}>
+        <Head>
+          <link rel="stylesheet" href="/style/reset.css" />
+        </Head>
+        <GlobalStyle />
+        {withoutAppLayout.includes(Component.name) ? <Component />
+          : (
+            <AppLayout>
+              <Component />
+            </AppLayout>
+          )}
+      </Provider>
+    </>
+  );
 Main.propTypes = {
-    Component: PropTypes.elementType,
+  Component: PropTypes.elementType.isRequired,
+  store: PropTypes.object.isRequired,
 };
 
+// eslint-disable-next-line no-unused-vars
 export default withRedux((initialState, options) => {
-    console.log('withRedux()...');
+  // eslint-disable-next-line no-console
+  console.log('withRedux()...');
 
-    const sagaMiddleware = createSagaMiddleware();
-    const middleware = [sagaMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [sagaMiddleware];
 
-    const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-    const store =  createStore(reducer, initialState, composeEnhancers(
-        applyMiddleware(...middleware),
-    ));
-    sagaMiddleware.run(rootSaga);
+  const store = createStore(reducer, initialState, composeEnhancers(
+    applyMiddleware(...middleware),
+  ));
+  sagaMiddleware.run(rootSaga);
 
-    return store;
-
+  return store;
 })(Main);
