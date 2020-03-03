d6b8b1ead7cbb15f8a07fd575cbab029745f9d0a
wrkbr <zcarc1@gmail.com>
2020-03-03 09:46:44 +0900

    댓글 컴포넌트 추가

---
+++ front/components/CommentLayout.js
@@ -0,0 +1,12 @@
+import {CommentField, CommentFieldInput, UploadBtn} from "./style/content";
+
+const ContentLayout = () => {
+    return(
+        <CommentField>
+            <CommentFieldInput type="text" size="70px" placeholder="댓글 달기..." maxLength="40"/>
+            <UploadBtn>게시</UploadBtn>
+        </CommentField>
+    );
+};
+
+export default ContentLayout;
--- front/pages/login.js
+++ front/pages/login.js
@@ -1,13 +1,11 @@
 import LoginLayout from "../components/LoginLayout";
 import {useEffect} from "react";
-import {useStore, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import Router from "next/router";
 
 const Login = () => {
 
     const {isLoggedIn} = useSelector(state => state.user);
-    // const store = useStore();
-    // const isLoggedIn = store.getState().user.isLoggedIn;
 
     useEffect(() => {
 
