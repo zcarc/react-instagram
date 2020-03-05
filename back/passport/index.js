1b74c434ebd78fd325e0d93733d0eaafc138168f
wrkbr <zcarc1@gmail.com>
2020-03-05 12:59:34 +0900

    로그인 패스포트 local callback 호출

--- back/passport/index.js
+++ back/passport/index.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const { Strategy: LocalStrategy } = require('passport-local');
 const db = require('../models/index');
+const bcrypt = require('bcrypt');
 
 module.exports = () => {
 
@@ -18,12 +19,30 @@ module.exports = () => {
                 where: {userId}
             });
 
-            console.log('#user.toJSON(): ', user && user.toJSON());
+            // console.log('#user: ', user);
+            // console.log('#user.id: ', user && user.id);
+            // console.log('#user.toJSON(): ', user && user.toJSON());
+
+            if(!user) {
+                return done(null, false,  { reason: '존재하지 않는 사용자입니다.' });
+            }
+
+            // result type: boolean
+            const result = await bcrypt.compare(userPassword, user.userPassword);
+            // console.log('#result: ', result);
+
+            if(result) {
+                return done(null, user);
+            }
+
+            console.log('비밀번호가 다릅니다.');
+            return done(null, false,  { reason: '비밀번호가 다릅니다.' });
 
             // done(null, { userId, userPassword });
 
         }catch (e) {
-
+            console.error(e);
+            return done(e);
         }
     }));
 
