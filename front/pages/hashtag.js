ca4ba0eea519cd870f45fb2dba53324e147712ca
wrkbr <zcarc1@gmail.com>
2020-03-13 10:16:43 +0900

    해쉬태그 페이지 SSR

--- front/pages/hashtag.js
+++ front/pages/hashtag.js
@@ -5,21 +5,12 @@ import {LOAD_HASHTAG_POSTS_REQUEST} from "../reducers/post";
 import ContentLayout from "../components/ContentLayout";
 import {ContentsBox, Inner} from "../components/style/content";
 
-const Hashtag = ( { tag } ) => {
+const Hashtag = () => {
     // console.log('Hashtag... pageProps: ', pageProps);
     // console.log('Hashtag... tag: ', tag);
 
-    const dispatch = useDispatch();
-
     const { mainPosts } = useSelector(state => state.post);
-    console.log('pages/hashtag... mainPosts: ', mainPosts);
-
-    useEffect(() => {
-        dispatch({
-          type: LOAD_HASHTAG_POSTS_REQUEST,
-          data: tag,
-        });
-    }, []);
+    // console.log('pages/hashtag... mainPosts: ', mainPosts);
 
     return (
         <>
@@ -32,16 +23,17 @@ const Hashtag = ( { tag } ) => {
     )
 };
 
-Hashtag.propTypes = {
-    tag: PropTypes.string.isRequired,
-};
-
 Hashtag.getInitialProps = (context) => {
     // console.log('Hashtag.getInitialProps... context: ', context);
     // console.log('Hashtag.getInitialProps... context.query: ', context.query);
-    // console.log('Hashtag.getInitialProps... context.query.tag: ', context.query.tag);
+    console.log('Hashtag.getInitialProps... context.query.tag: ', context.query.tag);
 
-    return { tag: context.query.tag };
+
+    context.store.dispatch({
+        type: LOAD_HASHTAG_POSTS_REQUEST,
+        data: context.query.tag,
+    });
+
 };
 
 export default Hashtag;
