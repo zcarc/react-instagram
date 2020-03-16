53eade85162400937e93a24ebb848ac1b8914844
wrkbr <zcarc1@gmail.com>
2020-03-16 16:01:22 +0900

    추가: compression-webpack-plugin

--- front/next.config.js
+++ front/next.config.js
@@ -1,18 +1,30 @@
 const withImages = require('next-images');
+const webpack = require('webpack');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
+
+const CompressionPlugin = require('compression-webpack-plugin');
+
 
 module.exports = withImages(withBundleAnalyzer({
     distDir: '.next',
 
     webpack(config) {
         const prod = process.env.NODE_ENV === 'production';
+        const plugins = [
+            ...config.plugins,
+            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+        ];
+        if(prod) {
+            plugins.push(prod && new CompressionPlugin());
+        }
         return {
             ...config,
             mode: prod ? 'production' : 'development',
             devtool: prod ? 'hidden-source-map' : 'eval',
+            plugins,
         };
     },
 }));
