de5b762b051c5e8916dd52583f9a29f0fc796f39
wrkbr <zcarc1@gmail.com>
2020-03-16 14:38:02 +0900

    @next/bundle-analyzer 설치 및 설정

--- front/next.config.js
+++ front/next.config.js
@@ -1,2 +1,18 @@
 const withImages = require('next-images');
-module.exports = withImages();
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withImages(withBundleAnalyzer({
+    distDir: '.next',
+
+    webpack(config) {
+        const prod = process.env.NODE_ENV === 'production';
+        return {
+            ...config,
+            mode: prod ? 'production' : 'development',
+            devtool: prod ? 'hidden-source-map' : 'eval',
+        };
+    },
+}));
